6961a22dd39caf7dbdfa4944ff438bbea606bea3
Daniel Tull <dt@danieltull.co.uk>
2021-11-04 13:19:03 +0000

    onAppear is called only the first time a view appears

--- OnAppearTest/ContentView.swift
+++ OnAppearTest/ContentView.swift
@@ -2,11 +2,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: some View {
+        OnAppearView()
+        FlipFlopView()
+    }
+}
+
+struct OnAppearView: View {
 
     @State private var value = 0
 
     var body: some View {
         Text("Hello #\(value)")
             .onAppear { value += 1 }
+    }
+}
+
+struct FlipFlopView: View {
+
+    @State private var value = 0
+
+    var body: some View {
+        if value.isMultiple(of: 2) {
+            Text("Even: \(value)")
+                .onAppear { value += 1 }
+        } else {
+            Text("Odd: \(value)")
+                .onAppear { value += 1 }
+        }
     }
 }
